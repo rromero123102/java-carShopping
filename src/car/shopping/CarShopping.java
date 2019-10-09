507c42ebe43736368121026500e366a016e02f47
Matthew Larrubia <mlarrubia@columbushs.com>
2019-10-09 10:23:56 -0400

    Starter Code

--- src/car/shopping/CarShopping.java
+++ src/car/shopping/CarShopping.java
@@ -5,17 +5,39 @@
  */
 package car.shopping;
 
-/**
- *
- * @author mlarrubia
- */
-public class CarShopping {
+// Import Scanner Class
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class CarShopping
+{
     
+    // Create static Scanner Object call it kb
+
+   public static void main(String[] args)
+   {
+      int totalCost = 0;
+      totalCost += base();
+      totalCost += automaticWindows();
+      //TODO: call methods that you write for each feature
+      //TODO: print the total cost of the vehicle
+   }
+   
+   public static int base()
+   {
+      System.out.println("Our inventory:");
+      System.out.println("Model 1 - $15,000");
+      System.out.println("Model 2 - $24,000");
+      System.out.println("Model 3 - $40,000");
+       System.out.println("Which base model did the customer choose?"
+               + "(enter full model name ex. Model 1): ");
+      String model = kb.nextLine();
+      if(model.equalsIgnoreCase("Model 1"))
+         return 15000;
+      //...
+   }
+   
+   public static int automaticWindows()
+   {
+      System.out.print("Automatic Windows for $500? (y/n) > ");
+      //...
+   }
 }
